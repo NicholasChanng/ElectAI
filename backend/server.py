a20c7bdfb035a7b89c4b7a4066284f502dca6950
Nicholas Channg <87846996+NicholasChanng@users.noreply.github.com>
2024-12-31 21:25:29 -0500

    test model

--- backend/server.py
+++ backend/server.py
@@ -5,10 +5,12 @@ import numpy as np
 app = Flask(__name__)
 
 model = joblib.load('voter_turnout_model.pkl')
+# input_vars = ['Year', 'Presidential', 'PercentBachelors', 'Income', 'PercentWhite', 'AverageAge']
+
 
 @app.route('/predict')
-def test():
-  return model.predict()
+def predict(year, presidential, percent_bachelors, income, percent_white, average_age):
+  return str(model.predict([[2024, 1, 40.0, 40.1, 70.1, 30.1]])[0])
 
 if __name__ == '__main__':
     app.run(debug=True)
